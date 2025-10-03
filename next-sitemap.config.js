24350a846d7c32a0409c02e177552294502db211
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-10-03 19:46:24 +0200

    .

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://trubaci-smederevo.rs", // Zameni svojim domenom
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+};
