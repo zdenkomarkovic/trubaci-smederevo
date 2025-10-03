5f23fdea16a4dfd691f2106d68debc6555a8f912
zdenkomarkovic <113395395+zdenkomarkovic@users.noreply.github.com>
2025-10-04 00:08:22 +0200

    .

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,12 @@ module.exports = {
   siteUrl: "https://trubaci-smederevo.rs",
   generateRobotsTxt: true,
   sitemapSize: 5000,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
 };
